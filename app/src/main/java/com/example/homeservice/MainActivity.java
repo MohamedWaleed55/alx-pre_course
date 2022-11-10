c7d691f96ec3787e89c696ec6790fe20d241c491
mohamed waleed <mowaled639@gmail.com>
2022-11-10 21:50:32 +0200

    hyhy

---
+++ app/src/main/java/com/example/homeservice/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.homeservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.xmldemon.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
